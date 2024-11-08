9c88a1858f4e0b61e4dc4c8050020e5c5f30293c
creativeboyzna <125282577+cretech20@users.noreply.github.com>
2024-11-08 22:51:38 +0530

    Version 1.0

--- OfferWeb.py
+++ OfferWeb.py
@@ -25,33 +25,54 @@ class OfferWeb(db.Model):
     s_date = db.Column(db.String(250), nullable=False)
     e_date = db.Column(db.String(250), nullable=False)
     featured = db.Column(db.Integer, nullable=False)
+    fe_date = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
     file_src = db.Column(db.String(250), unique=True, nullable=False)
 
 with app.app_context():
     db.create_all()
 
+def validity():
+    format = '%d-%m-%Y'
+    for offer in db.session.execute(db.select(OfferWeb)).scalars():
+        date_e = dt.datetime.strptime(offer.e_date, format).date()
+        date_s = dt.datetime.strptime(offer.s_date, format).date()
+        date_fe = dt.datetime.strptime(offer.fe_date, format).date()
+        today = dt.datetime.today().date()
+        if date_s > today or date_e < today:
+            offer.valid = 0
+        else:
+            offer.valid = 1
+            if date_fe < today:
+                offer.featured = 0
+    db.session.commit()
+
+
+
 @app.route("/")
 def home():
+    validity()
     feature = [fe_off for fe_off in db.session.execute(db.Select(OfferWeb)).scalars() if fe_off.featured == 1 and fe_off.valid == 1]
     stores = {store.store for store in db.session.execute(db.Select(OfferWeb)).scalars()}
     return render_template("home.html", featured=feature, stores=stores, year=dt.date.today().year)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", year=dt.date.today().year)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", year=dt.date.today().year)
 
 @app.route("/store/<name>")
 def store(name):
+    validity()
     offer_store = [offers for offers in db.session.execute(db.Select(OfferWeb)).scalars() if offers.valid == 1 and offers.store == name]
-    e_offer = [offers for offers in db.session.execute(db.Select(OfferWeb)).scalars() if offers.valid != 0 and offers.store == name]
-    return render_template("store.html", name=name, val=offer_store, exp=e_offer)
+    e_offer = [offers for offers in db.session.execute(db.Select(OfferWeb)).scalars() if offers.valid != 1 and offers.store == name]
+    return render_template("store.html", name=name, val=offer_store, exp=e_offer, year=dt.date.today().year)
 
 @app.route("/offer/<off_title>")
 def offer(off_title):
+    validity()
     offer = [offer for offer in db.session.execute(db.Select(OfferWeb)).scalars() if offer.valid == 1 and offer.offer_title == off_title][0]
-    return render_template("offer.html", offer=offer)
+    return render_template("offer.html", offer=offer, year=dt.date.today().year)
